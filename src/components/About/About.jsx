50a37cb9bb80611c75dc795902a9eb90458b6e25
Georgie Abreu <abreu.georgie@gmail.com>
2023-05-04 18:32:10 +0200

    création composant about

---
+++ src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import Header from '../Header';
+import Footer from '../Footer';
+
+function About() {
+    return (
+      <div className="About">
+        <Header/>
+        <Footer/>
+      </div>
+    );
+  }
+  
+  export default About;
