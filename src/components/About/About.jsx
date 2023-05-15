ef5d3e267e03e01823436a870adc1e4bc145bbfd
Georgie Abreu <abreu.georgie@gmail.com>
2023-05-15 22:16:35 +0200

    ajout de banner

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,22 +2,24 @@ import Header from '../Header';
 import Footer from '../Footer';
 import React from 'react';
 import Collapse from './Collapse';
+import Banner from '../Banner'
 
 function About() {
     return (
       <div className="About">
         <Header/>
+        <Banner/>
         <Collapse title="Fiabilité">
-        <p>@@@@@@</p>
+        <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
         </Collapse>
         <Collapse title="Respect">
-        <p>@@@@@@</p>
+        <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
         </Collapse>
         <Collapse title="Service">
-        <p>@@@@@@</p>
+        <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
         </Collapse>
         <Collapse title="Sécurité">
-        <p>@@@@@@</p>
+        <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
         </Collapse>
         <Footer/>
         </div>
