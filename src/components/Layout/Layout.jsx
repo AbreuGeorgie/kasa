99fe07832511c91af86da5b531327f584e6bdb9d
Georgie Abreu <abreu.georgie@gmail.com>
2023-05-26 09:24:42 +0200

    ajout favicon et titre personnalisé

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,8 +2,12 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import "../../style.scss";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 
 function Layout() {
+  useEffect(() => {
+    document.title = "Kasa";
+  }, []);
   return (
     <div className="page">
       <Header />
