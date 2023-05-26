ddbb141504641d849019bbe79059fb41a2b5c340
Georgie Abreu <abreu.georgie@gmail.com>
2023-05-26 09:30:55 +0200

    mise en page de Tag

--- src/components/Tag/Tag.jsx
+++ src/components/Tag/Tag.jsx
@@ -1,7 +1,14 @@
 import "./Tag.scss";
 
 function Tag({ tagName }) {
-  return <li className="tagList" key={tagName}>{tagName}</li>;
+  return (
+    <li
+      className="tagList"
+      key={tagName}
+    >
+      {tagName}
+    </li>
+  );
 }
 
 export default Tag;
